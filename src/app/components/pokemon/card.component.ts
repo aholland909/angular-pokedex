5d49f630a249f81fe570531bf4ca822c12d16348
Andrew Holland <andrewholland@C02F1NQTMD6M.local>
2022-11-11 11:47:48 +0000

    feat(pokemon card): bulbasaur pokemon card

---
+++ src/app/components/pokemon/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonDataType, PokemonType } from 'src/types/pokemon';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+
+export class PokemonCardComponent implements OnInit {
+
+  constructor(private pokemonService: PokemonService){}
+
+  pokemon:PokemonType = {
+    name: "",
+    height: 0,
+    weight: 0,
+    image: ""
+  }
+
+  ngOnInit() {
+    this.pokemonService.get('bulbasaur').subscribe((data) => {
+      this.pokemon = data;
+      console.log(this.pokemon)
+    })
+  }
+}
