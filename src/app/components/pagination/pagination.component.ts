ad41d725d10a76729857489842f5060c93fb93ba
Andrew Holland <andrewholland@C02F1NQTMD6M.local>
2022-11-23 14:50:09 +0000

    chore(merge main): merge main

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -4,23 +4,21 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 @Component({
   selector: 'app-pokemon-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.css']
+  styleUrls: ['./pagination.component.css'],
 })
-
 export class PokemonPaginationComponent implements OnInit {
-
-  constructor(private pokemonService: PokemonService){}
+  constructor(private pokemonService: PokemonService) {}
 
   page: number = 1;
   nextPageURL: string | null = null;
   previousPageURL: string | null = null;
 
   ngOnInit() {
-    this.pokemonService.nextPageURLEvent$.forEach(event => this.nextPageURL = event);
-    this.pokemonService.previousPageURLEvent$.forEach(event => this.previousPageURL = event);
+    this.pokemonService.nextPageURLEvent$.subscribe((event) => (this.nextPageURL = event));
+    this.pokemonService.previousPageURLEvent$.subscribe((event) => (this.previousPageURL = event));
   }
 
-  pageChange(newPage:number) {
-    this.page = this.pokemonService.pageChange(newPage)
+  pageChange(newPage: number) {
+    this.page = this.pokemonService.pageChange(newPage);
   }
 }
